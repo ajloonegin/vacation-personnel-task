2a7ca23e3040e4354aebbbbe0635f0d9f3f7af44
negin <ajloonegin@yahoo.com>
2024-02-18 15:24:59 +0330

    initial commit

---
+++ src/main/java/State2/Database.java
@@ -0,0 +1,32 @@
+package State2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Database {
+//    private static final JDBC_URL="jdbc:h2:~/test";
+//    private static final JDBC_DRIVER="org.h2.Driver";
+//    private static final USERNAME="Negin";
+//    private static final PASS="havij";
+    public static Connection con=null;
+
+    static{
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        if(con==null || con.isClosed()){
+            System.out.println("Connecting to database...");
+            con=DriverManager.getConnection("jdbc:h2:~/test","Negin","havij");
+        }
+        return con;
+    }
+    public void closeConnection() throws SQLException{
+        con.close();
+        System.out.println("Connection Close");
+    }
+
+}
--- src/main/java/State2/Run.java
+++ src/main/java/State2/Run.java
@@ -1,9 +1,6 @@
 package State2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.*;
 import java.util.Date;
 
@@ -16,36 +13,42 @@ public class Run {
 
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/mydb", "username",
-                    "password");
-            System.out.println("Connection established.");
-
-            String sql = "SELECT PERSONNELID FROM PERSONNEL WHERE NATIONALCODE =input";
-            PreparedStatement statement = con.prepareStatement(sql);
-            statement.setInt(1, 1);
-            ResultSet result = statement.executeQuery();
-            if (result.next()) {
-                int PERSONNELID = result.getInt("PERSONNELID");
+            Connection conn = Database.getConnection();
+            for(int i=1;i<=5;i++){
+                System.out.println(i+" ");
+                Thread.sleep(1000);
+            }
+            String sqlQuery1 = "SELECT PERSONNELID FROM PERSONNEL WHERE NATIONALCODE =input";
+            Statement stmt1 = conn.createStatement();
+            ResultSet rs1 = stmt1.executeQuery(sqlQuery1);
+            while (rs1.next()) {
+                int PERSONNELID = rs1.getInt("PERSONNELID");
                 System.out.println("id: " + PERSONNELID);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            String sql = "SELECT DATE FROM VACATION WHERE id = PERSONNELID";
-            PreparedStatement statement = con.prepareStatement(sql);
-            statement.setInt(1, 1);
-            ResultSet result = statement.executeQuery();
-            if (result.next()) {
-                Date DATE = result.getDate("DATE");
-
-                System.out.println("DATE: " + DATE);
+            String sqlQuery2 = "SELECT DATE FROM VACATION WHERE id = PERSONNELID";
+            Statement stmt2 = conn.createStatement();
+            ResultSet rs2 = stmt1.executeQuery(sqlQuery2);
+            while (rs2.next()) {
+                Date DATE = rs2.getDate("DATE");
+                System.out.println("id: " + DATE);
             }
+            new Database().closeConnection();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+//        void timer() {
+//            for(int i=1;i<=5;i++){
+//                System.out.println(i+" ");
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+
 
 
 
